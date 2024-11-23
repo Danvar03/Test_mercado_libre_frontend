46163429d180ccfd3fc18ea0a430d8729d0f027b
Daniela Vargas <daniela.vargas@sofka.com.co>
2024-11-22 19:55:37 -0500

    Revert "[wip] - Implement API for Paginated Item Retrieval with Axios Interceptor"
    This reverts commit 152af9d3437ebe526ccf67318a3483b8e0ebd5c9.

--- server/src/utils/extractCategories.js
+++
@@ -1,4 +0,0 @@
-export const extractCategories = (filters) => {
-  const categoryFilter = filters.find((filter) => filter.id === "category");
-  return categoryFilter?.values[0]?.path_from_root.map((cat) => cat.name) || [];
-};
