152af9d3437ebe526ccf67318a3483b8e0ebd5c9
Daniela Vargas <daniela.vargas@sofka.com.co>
2024-11-22 19:46:07 -0500

    [wip] - Implement API for Paginated Item Retrieval with Axios Interceptor

---
+++ server/src/utils/mapItem.js
@@ -0,0 +1,12 @@
+export const mapItem = (item) => ({
+  id: item.id,
+  title: item.title,
+  price: {
+    currency: item.currency_id,
+    amount: Math.floor(item.price),
+    decimals: Math.round((item.price % 1) * 100),
+  },
+  picture: item.thumbnail,
+  condition: item.condition,
+  free_shipping: item.shipping?.free_shipping || false,
+});
