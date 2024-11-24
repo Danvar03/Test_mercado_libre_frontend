b371e372614a7b404d9445b541ca3ea941a8c824
Daniela Vargas <daniela.vargas@sofka.com.co>
2024-11-24 08:42:13 -0500

    [refactor] - Update estructure initial

---
+++ ui-client-react/src/application/store.js
@@ -0,0 +1,25 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import searchSlice from "./slices/searchSlice";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: searchSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export default store;
