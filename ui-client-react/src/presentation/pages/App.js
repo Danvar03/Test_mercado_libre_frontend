bf4b4eb232e8dc09eff28f93243daceb18ad859f
Daniela Vargas <daniela.vargas@sofka.com.co>
2024-11-24 11:09:17 -0500

    [wip] - Implementation of Module Federation in the system and improvements in Webpack configuration

--- ui-client-react/src/presentation/pages/App.js
+++ ui-client-react/src/presentation/pages/App.js
@@ -1,9 +1,16 @@
 import "../styles/App.css";
+import React from "react";
+const Header = process.env.NODE_ENV === 'development'
+  ? React.lazy(() => import('remote/Header')) 
+  : null;
 
 function App() {
   return (
     <div className="App">
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Header />
       <h1> Mercado -Libre-Test</h1>
+      </React.Suspense>
     </div>
   );
 }
---
+++ ui-client-react/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index.js", 
+  output: {
+    path: path.resolve(__dirname, "dist"), 
+    filename: "main.js", 
+    publicPath: "/", 
+  },
+  mode: "development", 
+  devServer: {
+    port: 4000,
+    historyApiFallback: true, 
+    static: {
+      directory: path.join(__dirname, "public"), 
+    },
+    open: true, 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader", 
+        },
+      },
+      {
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf|otf)$/, 
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "container",
+        filename: "remoteEntry.js",
+        remotes: {
+          'remote': 'remote@http://localhost:4001/remoteEntry.js', 
+        },
+        exposes: {}, 
+      }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html", 
+      filename: "index.html", 
+    }),
+  ],
+};
